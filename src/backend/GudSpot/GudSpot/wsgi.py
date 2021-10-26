4b8e6788b5a601191017d9ca247f8484fe7ce2ec
ndkhoa0704 <58621544+ndkhoa0704@users.noreply.github.com>
2021-10-26 10:03:20 +0700

    Change environment, add api

--- src/backend/GudSpot/GudSpot/wsgi.py
+++ src/backend/GudSpot/GudSpot/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Gudspot project.
+WSGI config for GudSpot project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Gudspot.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GudSpot.settings')
 
 application = get_wsgi_application()
--- src/backend/GudSpot/manage.py
+++ src/backend/GudSpot/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Gudspot.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GudSpot.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
