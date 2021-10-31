ae393ec57c43f0fb6cf0ea98098d5f3eecb4520d
Nguyễn Năng Tiến <tien.nnang@gmail.com>
2021-10-31 19:42:05 +0700

    Change email to user + add Logo

--- src/frontend/src/pages/LogIn/index.js
+++ src/frontend/src/pages/LogIn/index.js
@@ -14,10 +14,10 @@ const LogIn = () => {
                 <div class="w-full md:max-w-xl border border-gray-200 bg-white shadow-lg rounded px-8 pt-8 pb-6">
                     <form class="mb-4">
                         <div class="mb-8">
-                            <label class="block text-gray-700 text-sm md:text-base font-bold mb-2" for="email">
-                                Email
+                            <label class="block text-gray-700 text-sm md:text-base font-bold mb-2" for="username">
+                                Username
                             </label>
-                            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Quamon@example.com" required/>
+                            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="Quamon" required/>
                         </div>
                         <div class="mb-8">
                             <label class="block text-gray-700 text-sm md:text-base font-bold mb-2" for="password">
--- src/frontend/src/pages/Register/index.js
+++ src/frontend/src/pages/Register/index.js
@@ -13,8 +13,8 @@ const Register = () => {
                 <div class="w-full md:max-w-xl border border-gray-200 bg-white shadow-lg rounded px-8 pt-8 pb-6">
                     <form class="mb-4">
                         <div class="mb-3">
-                            <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
-                                Name
+                            <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                                Username
                             </label>
                             <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="Nguyen Van A" required/>
                         </div>
