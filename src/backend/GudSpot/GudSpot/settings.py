cfb9cb870882197ca13482481671afd89076cc21
koanguyn <ndkhoa0704@gmail.com>
2021-12-06 20:54:06 +0700

    Add script

--- src/backend/GudSpot/GudSpot/settings.py
+++ src/backend/GudSpot/GudSpot/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME':'GudSpot', 
-        'USER': 'django',
-        'PASSWORD': 'Password123#@!',
+        'USER': 'root',
+        'PASSWORD': 'password',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
