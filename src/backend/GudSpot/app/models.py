0f44e48de8d05e3fa83b4be488026858f3d3b6e9
Datnguyen2901 <datnguyen2901@gmail.com>
2021-12-31 16:24:49 +0700

    models update

--- src/backend/GudSpot/app/models.py
+++ src/backend/GudSpot/app/models.py
@@ -1,7 +1,13 @@
+from enum import unique
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import TextField
 from django.db.models.fields.related import ForeignKey
 from knox.models import AuthToken
+from django.contrib.auth.models import User
+
 # Create your models here.
+
 
 class user_type(models.Model):
     class USER_TYPES(models.IntegerChoices):
@@ -9,5 +15,67 @@ class user_type(models.Model):
         OWNER = 1
         R_USERS = 2
         USERS = 3
-    user_id = models.IntegerField(primary_key=True,ForeignKey=auth_user)
+
+    user_id = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
     user_type = models.IntegerField(choices=USER_TYPES.choices)
+
+
+# Store model
+class Store(models.Model):
+    owner_id = models.ForeignKey(User, on_delete=CASCADE)
+    store_name = models.CharField(max_length=100)
+    store_address = models.CharField(max_length=200)
+    create_date = models.DateTimeField()
+    img_url = models.URLField()
+
+
+# Blog model
+class Blog(models.Model):
+    content = models.TextField()
+    img_url = models.URLField()
+    posted_date = models.DateTimeField()
+
+
+# Review model
+class Review(models.Model):
+    class Meta:
+        unique_together = (("store_id", "user_id"),)
+
+    REVIEW_SCORE = [
+        (1, "TooBad"),
+        (2, "Bad"),
+        (3, "Neutral"),
+        (4, "Good"),
+        (5, "VeryGood"),
+    ]
+    store_id = models.ForeignKey(Store, on_delete=CASCADE)
+    user_id = models.ForeignKey(User, primary_key=True, on_delete=CASCADE)
+    score = models.IntegerField(choices=REVIEW_SCORE)
+
+
+# Comment model
+class Comment(models.Model):
+    class Meta:
+        unique_together = (("blog_id", "user_id"),)
+
+    blog_id = models.ForeignKey(Blog, on_delete=CASCADE)
+    user_id = models.ForeignKey(User, primary_key=True, on_delete=CASCADE)
+    content = models.TextField()
+
+
+# Favorite model
+class Favorite(models.Model):
+    class Meta:
+        unique_together = (("store_id", "user_id"),)
+
+    store_id = models.ForeignKey(Store, on_delete=CASCADE)
+    user_id = models.ForeignKey(User, primary_key=True, on_delete=CASCADE)
+
+
+# Follow model
+class Follow(models.Model):
+    class Meta:
+        unique_together = (("store_id", "user_id"),)
+
+    store_id = models.ForeignKey(Store, on_delete=CASCADE)
+    user_id = models.ForeignKey(User, primary_key=True, on_delete=CASCADE)
