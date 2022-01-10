26f8c9fc7654ea26b85a2eb3afab1072e39906bd
Datnguyen2901 <datnguyen2901@gmail.com>
2022-01-10 19:41:03 +0700

    admin site for store and blog

--- src/backend/GudSpot/GudSpot/settings.py
+++ src/backend/GudSpot/GudSpot/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "GudSpot",
         "USER": "root",
-        "PASSWORD": "password",
+        "PASSWORD": "datnguyen2901",
         "HOST": "127.0.0.1",
         "PORT": "3306",
     }
